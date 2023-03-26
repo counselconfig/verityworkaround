b8895944102fd3f352526c4c5c882289fff6deab
Dominic <you@example.com>
2023-03-26 00:36:11 +0000

    first commit

---
+++ Default.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="VerityWebApp._Default" %> <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+  <div class="jumbotron">
+    <h3>1. Create an empty edit set on test <a href="http://test.editorial.tna.local/editorial/" target="_blank" rel="noopener">PROCAT</a><br /><br />2. Use this form to add pieces to the edit set</h3>
+    <p class="lead"></p>
+  </div>
+  <div class="row">
+    <div class="col-md-9">
+      <h2>Add piece information</h2>
+      <p></p>
+      <table style="width: 100%;">
+
+        <tr >
+          <td class="text-left" style="height: 50px">Letter code: *</td>
+          <td style="height: 50px">
+            <asp:DropDownList ID="cboLetterCode" runat="server" ValidationGroup="AddPieceGroup" DataSourceID="SqlDataSourceILDB" DataTextField="letter_code" DataValueField="lettercode_id" AutoPostBack="True" OnSelectedIndexChanged="cboLetterCode_SelectedIndexChanged" AppendDataBoundItems="true">
+              <asp:ListItem Selected="True" Text="--Select Letter Code--" disabled="disabled"></asp:ListItem>
+            </asp:DropDownList>
+            <asp:SqlDataSource ID="SqlDataSourceILDB" runat="server" ConnectionString="<%$ ConnectionStrings:ILDBConnectionString %>" SelectCommand="SELECT DISTINCT [lettercode_id],[letter_code]  FROM [ILDB].[dbo].[tbl_lettercode]">
+            </asp:SqlDataSource>
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-left" style="height: 50px">Class number: *</td>
+          <td style="height: 50px">
+            <asp:DropDownList ID="cboClass" runat="server" ValidationGroup="AddPieceGroup" DataTextField="class_no" DataValueField="class_no"></asp:DropDownList>
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-left" style="height: 50px">First piece: * 
+          </td>
+          <td style="height: 50px">
+            <asp:TextBox ID="TxtFirstPiece" runat="server"></asp:TextBox>
+              <asp:RegularExpressionValidator asp:RequiredFieldValidatorID="FirstPiece" runat="server" ControlToValidate="TxtFirstPiece" ErrorMessage="Only numbers, lower or upper case, hyphens, and forward slashes allowed" ForeColor="Red" ValidationExpression="^[0-9A-Za-z/-]+$"></asp:RegularExpressionValidator>
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-left" style="height: 50px">Last piece: 
+          </td>
+          <td style="height: 50px">
+            <asp:TextBox ID="TxtLastPiece" runat="server"></asp:TextBox>&nbsp; 
+              <asp:RegularExpressionValidator ID="LastPieceNumber" ControlToValidate="TxtFirstPiece" runat="server" ErrorMessage="Only numbers, lower or upper case, hyphens, and forward slashes allowed" ForeColor="Red" ValidationExpression="^[0-9A-Za-z/-]+$"></asp:RegularExpressionValidator>
+          </td>
+        </tr>
+
+        <tr>
+          <td class="text-left" style="height: 50px">Edit set name: * 
+          </td>
+          <td style="height: 50px">
+             <asp:TextBox ID="TxtEditSet" placeholder="Enter name from PROCAT" runat="server" ></asp:TextBox>&nbsp;<asp:RegularExpressionValidator ID="EditSetValidator"  ControlToValidate="TxtEditSet" runat="server"  ErrorMessage="Editset name disallowed" ForeColor="Red" ValidationExpression="[\s\S]*"> </asp:RegularExpressionValidator>
+            <asp:RequiredFieldValidator asp:RequiredFieldValidatorID="EditSet" runat="server" ControlToValidate="TxtEditSet" ErrorMessage="* Please enter edit set" ForeColor="Red" ID="RequiredFieldValidator2"></asp:RequiredFieldValidator>
+
+          </td>
+        </tr>
+
+        <tr>
+           <td style="height: 50px">
+            <asp:Button ID="btnAddPiece" runat="server" Text="Populate Edit Set" OnClick="btnAddPiece_Click" CausesValidation="true" />
+          </td>
+        </tr>
+
+      </table>
+    </div>
+  </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace VerityWebApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        
+
+        protected void btnAddPiece_Click(object sender, EventArgs e)
+        {
+
+            //if (TxtFirstPiece.Text != "" && TxtEditSet.Text != "" && cboClass.Text != "")
+            //{
+
+            //string connString = @"server=na-t-sqlc02v02\sql2; database=ILDB;uid=sa;pwd=jW76S0@qjmz*2";
+            //string connString = @"Data Source=na-t-sqlc02v02\sql2;Initial Catalog=ILDB; User id=sa; Password=jW76S0@qjmz*2; Integrated Security=False";
+
+            //sql connection object
+            SqlConnection cnx = new SqlConnection(ConfigurationManager.ConnectionStrings["ILDBConnectionString"].ConnectionString);
+            {
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "usp_Edit_InsertPiecesToEditSet";
+                    cmd.Parameters.Add("@lettercode", SqlDbType.NVarChar).Value = cboLetterCode.SelectedItem.Text;
+                    cmd.Parameters.Add("@class_no", SqlDbType.Int).Value = cboClass.SelectedItem.Text;
+                    cmd.Parameters.Add("@minPRef", SqlDbType.NVarChar).Value = TxtFirstPiece.Text.Trim();
+                    if (!string.IsNullOrWhiteSpace(TxtLastPiece.Text))
+                    {
+                        cmd.Parameters.Add("@maxPRef", SqlDbType.NVarChar).Value = TxtLastPiece.Text.Trim();
+                    }
+                    else
+                    {
+                        cmd.Parameters.Add("@maxPRef", SqlDbType.NVarChar).Value = DBNull.Value;
+                    }
+
+                    cmd.Parameters.Add("@editSetName", SqlDbType.NVarChar).Value = TxtEditSet.Text.Trim();
+                    cmd.Connection = cnx;
+
+                    try
+                    {
+                        cnx.Open();
+                        cmd.ExecuteNonQuery();
+                        Response.Write("<script>window.alert('Successfully updated. Please verify!');</script>");
+                    }
+                    catch (Exception ex)
+                    {
+                        Response.Write("<script>window.alert('Import error, please contact IT Service Desk. ');</script>");
+                        //throw ex;
+
+                    }
+                    finally
+                    {
+                        cnx.Close();
+                        ClearControls();
+                    }
+
+                }
+            //}
+            //else
+            //{
+            //    Response.Write("<script>window.alert('* marked fields are mandatory');</script>");
+            //}
+
+        }
+        
+        protected void ClearControls()
+        {
+            cboLetterCode.DataBind();
+            cboClass.Items.Clear();
+            TxtEditSet.Text = "";
+            TxtFirstPiece.Text = "";
+            TxtLastPiece.Text = "";
+            
+        }
+
+        protected void cboLetterCode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TxtFirstPiece.Text = "";
+            TxtLastPiece.Text = "";
+            cboClass.Items.Clear();
+            SqlDataReader dReader;
+
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ILDBConnectionString"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT class_no, class_id FROM tbl_class WHERE lettercode_id IN (" + cboLetterCode.SelectedValue + ")", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+                con.Open();
+
+                dReader = cmd.ExecuteReader();
+                if (dReader.HasRows == true)
+                {
+                    cboClass.DataSource = dReader;
+                    cboClass.DataTextField = "class_no";
+                    cboClass.DataValueField = "class_id";
+                    cboClass.DataBind();
+                    con.Close();
+
+                }
+                else
+                {
+                    cboClass.Text = "--No Class found--";
+                }
+                //dReader.Close();
+                cboClass.Items.Insert(0, new ListItem("--Select Class--", "0"));
+               
+            }
+
+
+        }
+
+      
+    }
+}
